0f51f137bb665dc9dbf19bf5f5937a24a9ad3cc7
Ramona <rare2400@student.miun.com>
2025-11-13 17:22:34 +0100

    skapar js för att trycka fram navmeny på tailwind-sidor

---
+++ src/js/toggleMenu.js
@@ -0,0 +1,7 @@
+//toggle menu on pages styled with Tailwind CSS
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+});
